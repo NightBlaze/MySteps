c48857252caf58f847c0f049d8b362fc8128ce38
Alexander Timonenkov <nightblaze1@gmail.com>
2020-06-05 21:23:56 +0300

    Initial project setup

---
+++ MySteps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySteps
+//
+//  Created by Alexander Timonenkov on 05.06.2020.
+//  Copyright © 2020 ATi Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
